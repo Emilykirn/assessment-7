37001ac5e9e0518b7a7590a5ffb334d494e0f1be
Emily Kirn <emilykirn@gmail.com>
2022-12-02 16:23:35 -0700

    corrected

--- Step2.js
+++ Step2.js
@@ -1,64 +1,104 @@
 // 1) Sum Zero
 
-let array = []
 
-function twoNums (array){
- if(array[i] + array[i] === 0){
-    return true
- } else{
-     return false
+function addToZero (nums){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(i !== j){
+                if(nums[i] + nums[j] === 0){
+                    return true
+                }
+            }
+        }
+
+    }
+    return false
  }
-}
-// RUNTIME: O(1)
+
+console.log(addToZero([]))
+
+console.log(addToZero([1]))
+
+console.log(addToZero([1,2,3]))
+
+console.log(addToZero([1,,2,3,-2]))
+
+// TIME COMPLEXCITY: O(n^2)
 // SPACE COMPLEXITY: O(1)
 
 // 2) Unique Characters
 
-function hasUniqueChars(word) {
-    let uniqueChars = new Set([])
-    for (let i = 0; i < word.length; i++) {
-      uniqueChars.add(word[i])
-    }
-    return uniqueChars.size === word.length
+  function hasUniqueChars(word) {
+      for(let i = 0; i < word.length; i++){
+          for(let j = i + 1; i < word.length; j++){
+              if(word[i] === word[j]){
+                  return false
+              }
+          }
+
+      }
+      return true
   }
 
-// RUNTIME: O(N)
-// SPACE COMPLEXITY: O(N)
+  console.log(hasUniqueChars('monday'))
+  console.log(hasUniqueChars('moonday'))
+
+// TIME COMPLEXITY: O(n^2)
+// SPACE COMPLEXITY: O(n)
 
 // 3) Pangram Sentence
 
-function isPangram(string){
-    let strArr = string.toLowerCase();
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// function isPangram(string){
+//     let strArr = string.toLowerCase();
+//     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     
-    for (let i = 0; i < alphabet.length; i++) {
-      if(strArr.indexOf(alphabet[i]) < 0){
-        return false;
-      }
-    }
-    return true;
-  }
+//     for (let i = 0; i < alphabet.length; i++) {
+//       if(strArr.indexOf(alphabet[i]) < 0){
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
 
-  // RUNTIME: O(N)
+  // TIME COMPLEXITY: O(N)
   // SPACE COMPLEXITY: O(N)
 
+function isPangram(str){
+    let alphaArr = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    str = str.toLowerCase()
+
+    for( let i = 0; i < str.length; i++){
+        let letter = str[i]
+        let index = alphaArr.indexOf(letter)
+        if(index !== -1){
+            alphaArr.splice(index, 1)
+        }
+    }
+    if(alphaArr.length === 0){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(isPangram('The quick brown fox jumps over the lazy dog!'))
+console.log(isPangram("I like cats, but not mice"))
+
+// TIME COMPLEXCITY: O(N)
+// SPACE COMPLEXITY: O(N)
 
   // 4) Longest Word
 
-  function longestWord(string) {
-    var str = string.split(" ");
-    var longest = 0;
-    var word = null;
-    for (var i = 0; i < str.length - 1; i++) {
-        if (longest < str[i].length) {
-            longest = str[i].length;
-            word = str[i];
+function findLongestWord(arr){
+    let biggest = 0;
+    for( let i =0; i < arr.length; i++){
+        if(arr[i].length > biggest){
+                biggest = arr[i].length
         }
     }
-    return word;
+    return biggest
 }
-
-  // RUNTIME:  O(n^2)
+console.log(findLongestWord(['hi','hello', 'cat']))
+  // RUNTIME:  O(n)
   // SPACE COMPLEXITY: O(n^2)
  
 
